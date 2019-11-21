25856f45b45a2ba66cd9e3ceb375425a100149ba
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2019-11-21 00:18:30 -0600

    dd Hot Air Balloons

---
+++ Reference Projects/Flight Test/Assets/Scripts/BalloonController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonController : MonoBehaviour {
+
+	/*--- Variable Declarations ---*/
+
+	// Public Variables
+	public float verticalOffset = 5f;
+	public float temporalOffset = 0f;
+	public float cycleLength = 5f;
+
+	// Private Variables
+	private float currentTime = 0f; 
+	private Vector3 initialPosition;
+
+
+	/*--- Lifecycle Methods ---*/
+
+    void Start() {
+        
+        // Get Initial Position
+        initialPosition = transform.position;
+    }
+
+    void FixedUpdate() {
+        
+        // Update Time
+        currentTime += Time.deltaTime;
+
+        // Calculate Position
+        float factor = currentTime + temporalOffset;
+        float normalizedFactor = factor * (Mathf.PI / (1.5f * cycleLength));
+        float positionOffsetFactor = .5f * (1 + Mathf.Sin(3f * normalizedFactor - Mathf.PI / 2f));
+
+        // Update Position
+        transform.position = new Vector3(
+        	initialPosition.x,
+        	initialPosition.y + (positionOffsetFactor * verticalOffset),
+        	initialPosition.z
+        );
+    }
+
+}
